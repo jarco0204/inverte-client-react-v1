85e296a3af85b19e0aff00481c5abaa44bbaaf64
Johan Arcos Mendez <jarco0204@outlook.com>
2021-03-21 20:07:38 -0500

    add(client): send data to api

--- client/src/components/SignIn/index.js
+++ client/src/components/SignIn/index.js
@@ -1,44 +1,83 @@
-import React, {useState} from "react";
-import {useHistory} from "react-router-dom";
-import {SignInContainer, SignInContent, SignInForm, FormTitle, FormInputLabel, FormInput, FormSubmitButton} from "./SignInElements"
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import {
+    SignInContainer,
+    SignInContent,
+    SignInForm,
+    FormTitle,
+    FormInputLabel,
+    FormInput,
+    FormSubmitButton,
+} from "./SignInElements";
 
+//Globals
+let APIURL = "http://localhost:8000";
 const SignIn = () => {
     const [emailInputValue, setEmailInputValue] = useState("");
     const [passwordInputValue, setPasswordInputValue] = useState("");
     const history = useHistory();
 
-    const handleInputChange = (event) =>{
-        if(event.target.id ==="email"){
-            setEmailInputValue(event.target.value)
+    const handleInputChange = (event) => {
+        if (event.target.id === "email") {
+            setEmailInputValue(event.target.value);
+        } else if (event.target.id === "password") {
+            setPasswordInputValue(event.target.value);
         }
-        else{
-            setPasswordInputValue(event.target.value)
-        }
-        
-    }
-    const validateForm = (event)=>{
+    };
+
+    const validateForm = async (event) => {
         event.preventDefault();
-        console.log(emailInputValue);
-        history.push("/newPath")
-    }
+        let dataToSend = {
+            emailIn: emailInputValue,
+            passIn: passwordInputValue,
+        };
+        console.log(dataToSend);
+        await axios
+            .post(APIURL + "/api/user/validate", dataToSend, {
+                headers: {
+                    "Content-Type": "application/json;charset=UTF-8",
+                    "Access-Control-Allow-Origin": APIURL,
+                },
+            })
+            .then((response) => {
+                console.log(response);
+                history.push("/newPath");
+            })
+            .catch((response) => {
+                console.log(response);
+                history.push("/");
+            });
+    };
+
     return (
         <SignInContainer>
-        <SignInContent>
-            <SignInForm >
-                <FormTitle>
-                    Sign-In to make use of our web-app
-                </FormTitle>
-                <FormInputLabel htmlFor="email">
-                    Email:
-                </FormInputLabel>
-                <FormInput id="email" value= {emailInputValue} required type="email" onChange={handleInputChange}></FormInput>
-                <FormInputLabel htmlFor="password">
-                    Password:
-                </FormInputLabel>
-                <FormInput id="password" value= {passwordInputValue} required type="password" onChange={handleInputChange}></FormInput>
-                <FormSubmitButton onClick={validateForm}>Sign-in</FormSubmitButton>
-            </SignInForm>
-        </SignInContent>
+            <SignInContent>
+                <SignInForm>
+                    <FormTitle>Sign-In to make use of our web-app</FormTitle>
+                    <FormInputLabel htmlFor="email">Email:</FormInputLabel>
+                    <FormInput
+                        id="email"
+                        value={emailInputValue}
+                        required
+                        type="email"
+                        onChange={handleInputChange}
+                    ></FormInput>
+                    <FormInputLabel htmlFor="password">
+                        Password:
+                    </FormInputLabel>
+                    <FormInput
+                        id="password"
+                        value={passwordInputValue}
+                        required
+                        type="password"
+                        onChange={handleInputChange}
+                    ></FormInput>
+                    <FormSubmitButton onClick={validateForm}>
+                        Sign-in
+                    </FormSubmitButton>
+                </SignInForm>
+            </SignInContent>
         </SignInContainer>
     );
 };
